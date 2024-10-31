d9b37eab7f00ba818535b2371e6ccfccbac240a8
Kayla Perez <kayla.n.perez404@gmail.com>
2024-10-31 16:11:48 -0700

    renamed for clarity

---
+++ django-backend/backend/notes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Notes(models.Model):
+	COURSE_CHOICES = {
+		"CSE123" : "CSE123: Programming Fundamentals I ",
+		"CSE124" : "CSE124: Programming Fundamentals II",
+		"MATH307": "MATH307: Differential Equations",
+		"MATH308": "MATH308: Linear Algebra",
+		"ENGL204": "ENGL204: Popular Fiction and Media" 
+
+	}
+	course = models.CharField(choices=COURSE_CHOICES)
+	instructor = models.CharField(max_length=30)
+	lectureNum = models.IntegerField() 
+	notes = models.TextField()
