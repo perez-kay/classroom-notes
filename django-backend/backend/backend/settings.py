63a9cffc666946d193aa0d0d16fe8de10e77026b
Kayla Perez <kayla.n.perez404@gmail.com>
2024-10-31 16:41:02 -0700

    added packages to settings

--- django-backend/backend/backend/settings.py
+++ django-backend/backend/backend/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'rest_framework',
+	'corsheaders',
 	'notes'
 ]
 
@@ -125,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
