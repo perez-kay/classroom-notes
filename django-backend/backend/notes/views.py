1d16cfd22d29501d324ed90a37eeba093a16b124
Kayla Perez <kayla.n.perez404@gmail.com>
2024-10-31 17:04:02 -0700

    added permissions

--- django-backend/backend/notes/views.py
+++ django-backend/backend/notes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+from notes.permissions import IsOwnerOrReadOnly
+from .serializers  import NoteSerializer, UserSerializer
+from .models import Note
 
-# Create your views here.
+class NoteViewSet(viewsets.ModelViewSet):
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+	serializer_class = NoteSerializer
+	queryset = Note.objects.all()
+
+	def perform_create(self, serializer):
+		serializer.save(author=self.request.user)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
