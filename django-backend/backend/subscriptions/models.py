23bc30872ecd0487d8e3e72df0f5946e7d5aea19
Kayla Perez <kayla.n.perez404@gmail.com>
2024-11-07 15:03:23 -0800

    reordered cols

--- django-backend/backend/subscriptions/models.py
+++ django-backend/backend/subscriptions/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Subscription(models.Model):
-	subscribed_to = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
 	subscriber = models.ForeignKey('students.Student', on_delete=models.CASCADE)
+	subscribed_to = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
 
 	def __str__(self):
 		return f"{self.subscriber.user.username} is subscribed to {self.subscribed_to.code}"
