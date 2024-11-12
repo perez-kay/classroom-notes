eace888a1d22541411d2d310022f82f357c8c317
Kayla Perez <kayla.n.perez404@gmail.com>
2024-11-12 14:09:01 -0800

    got grid for notes working nice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Note from './components/Note';
 import axios from 'axios';
 
 function App() {
@@ -13,30 +17,31 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {notes.map((note) => (
-        <Note
-          author={note.author}
-          course={note.course}
-          instructor={note.instructor}
-          lectureNum={note.lectureNum}
-          notes={note.notes}
-        />
-      ))}
-    </div>
+    <Container>
+      <Row>
+        {notes.map((note) => (
+          <Note
+            author={note.author}
+            course={note.course}
+            title={note.title}
+            description={note.description}
+          />
+        ))}
+      </Row>
+    </Container>
   );
 }
 
-function Note({ author, course, instructor, lectureNum, notes }) {
-  return (
-    <div>
-      <h1>Course: {course}</h1>
-      <h3>Instructor: {instructor}</h3>
-      <h4>Lecture {lectureNum}/11</h4>
-      <h5>Notes Author: {author}</h5>
-      <p>{notes}</p>
-    </div>
-  );
-}
+// function Note({ author, course, instructor, lectureNum, notes }) {
+//   return (
+//     <div>
+//       <h1>Course: {course}</h1>
+//       <h3>Instructor: {instructor}</h3>
+//       <h4>Lecture {lectureNum}/11</h4>
+//       <h5>Notes Author: {author}</h5>
+//       <p>{notes}</p>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ frontend/src/components/Note.js
@@ -0,0 +1,18 @@
+import styles from './Note.module.css';
+import Card from 'react-bootstrap/Card';
+
+function Note({ author, course, title, description }) {
+  return (
+    <div className="col-md-4 col-sm-6">
+      <Card className={`${styles.noteCard} m-2`}>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Subtitle>{course}</Card.Subtitle>
+          <Card.Text>{description}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default Note;
