36db849b714f60c6ed7a08ef379f4db84e4df173
Kayla Perez <kayla.n.perez404@gmail.com>
2024-11-12 13:29:11 -0800

    changed to just represent a User object

--- django-backend/backend/students/serializers.py
+++ django-backend/backend/students/serializers.py
@@ -1,12 +1,13 @@
-from .models import Student
+# from .models import Student
+from django.contrib.auth.models import User
 from notes.models import Note
 from rest_framework import serializers
 
 class StudentSerializer(serializers.ModelSerializer):
-	written_notes = serializers.PrimaryKeyRelatedField(many=True, queryset=Note.objects.all())
+	# TODO: get this data
+	# written_notes = serializers.PrimaryKeyRelatedField(many=True, queryset=Note.objects.all())
 	# TODO: look into https://www.django-rest-framework.org/api-guide/relations/ for better ways to serialize this relationship.
 
 	class Meta:
-		model = Student
-		# TODO: figure out whatr the user.username field would be represented as, as user returns the id number and user.username doesn't work.
-		fields = ['id', 'user', 'written_notes']
+		model = User
+		fields = ['id', 'username']
