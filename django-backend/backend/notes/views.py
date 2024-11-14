1f076e130bf9748b9cba52d76e5f1a0de3f2e64a
Kayla Perez <kayla.n.perez404@gmail.com>
2024-11-13 16:36:32 -0800

    ordered notes by most recent

--- django-backend/backend/notes/views.py
+++ django-backend/backend/notes/views.py
@@ -6,7 +6,7 @@ from .models import Note
 class NoteViewSet(viewsets.ModelViewSet):
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 	serializer_class = NoteSerializer
-	queryset = Note.objects.all()
+	queryset = Note.objects.all().order_by('-id')
 
 	def perform_create(self, serializer):
 		# Can POST with just basic django user. this might be the right way to do it.
