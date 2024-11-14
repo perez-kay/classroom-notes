20f117ae69f64ab21ddd870076d3666ca5203405
Kayla Perez <kayla.n.perez404@gmail.com>
2024-11-13 16:38:00 -0800

    styled note card a bit more

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,14 +1,22 @@
 import styles from './Note.module.css';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/esm/Button';
+import { Link } from 'react-router-dom';
 
-function Note({ author, course, title, description }) {
+function Note({ id, author, course, title, description }) {
+  const shortText = description.split(' ').slice(0, 15).join(' ') + '...';
+
   return (
-    <div className="col-md-4 col-sm-6">
+    <div className="col-md-4 col-sm-6 d-flex align-items-stretch">
       <Card className={`${styles.noteCard} m-2`}>
         <Card.Body>
           <Card.Title>{course}</Card.Title>
-          <Card.Subtitle>{title}</Card.Subtitle>
-          <Card.Text>{description}</Card.Text>
+          <Card.Subtitle className="text-muted">{title}</Card.Subtitle>
+          <p className="fs-6 text-muted mb-0">{author}</p>
+          <Card.Text>{shortText}</Card.Text>
+          <Link to={`/notes/${id}`}>
+            <Button variant="primary">See More</Button>
+          </Link>
         </Card.Body>
       </Card>
     </div>
