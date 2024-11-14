d766a2e51189ccb8b1509cea5c0e5a0b8fa6ac7a
Kayla Perez <kayla.n.perez404@gmail.com>
2024-11-13 16:37:23 -0800

    added routing

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,9 @@
 import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 import Accordion from 'react-bootstrap/Accordion';
+import { NavLink } from 'react-router-dom';
 
 import styles from './NavBar.module.css';
 import { useAccordionButton } from 'react-bootstrap/esm/AccordionButton';
@@ -7,39 +11,58 @@ import { useState } from 'react';
 
 function NavBar() {
   return (
-    <Nav
-      className={`${styles.sidebar}  m-0 flex-column vh-100`}
-      defaultActiveKey={'/'}
-    >
-      <h1 className="text-center p-4">Sharing Notes</h1>
+    <nav className="position-sticky">
+      <Nav
+        className={`${styles.sidebar}  m-0 px-1 flex-column vh-100`}
+        defaultActiveKey={'/'}
+      >
+        <h1 className="text-center px-4 pt-4">Sharing Notes</h1>
+        <hr />
 
-      <Nav.Link href="/">
-        <i class="bi bi-house"></i> Home
-      </Nav.Link>
+        <SearchBar />
 
-      <Nav.Link>
-        <i class="bi bi-person-circle"></i> Profile
-      </Nav.Link>
+        <Nav.Item>
+          <NavLink to="/" className="ps-3 py-2 d-block">
+            <i className="bi bi-house fs-5 pe-2"></i>Home
+          </NavLink>
+        </Nav.Item>
 
-      <Nav.Link>
-        <i class="bi bi-journal"></i> My Notes
-      </Nav.Link>
+        <Nav.Item>
+          <NavLink to="/profile" className="ps-3 py-2 d-block">
+            <i className="bi bi-person-circle fs-5 pe-2"></i> Profile
+          </NavLink>
+        </Nav.Item>
 
-      <Accordion flush>
-        <CoursesToggle eventKey="courses" />
-        <Accordion.Collapse eventKey="courses">
-          <div className="ps-4">
-            <Nav.Link>Course 1</Nav.Link>
-            <Nav.Link>Course 2</Nav.Link>
-            <Nav.Link>Course 2</Nav.Link>
-          </div>
-        </Accordion.Collapse>
-      </Accordion>
+        <Nav.Item>
+          <NavLink to="/my-notes" className="ps-3 py-2 d-block">
+            <i className="bi bi-journal fs-5 pe-2"></i> My Notes
+          </NavLink>
+        </Nav.Item>
 
-      <Nav.Link>
-        <i class="bi bi-pencil-square"></i> Share a New Note
-      </Nav.Link>
-    </Nav>
+        <Accordion flush>
+          <CoursesToggle eventKey="courses" />
+          <Accordion.Collapse eventKey="courses">
+            <div className="ps-4">
+              <Nav.Item>
+                <NavLink to="/courses/course1">Course 1</NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink to="/courses/course2">Course 2</NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink to="/courses/course3">Course 3</NavLink>
+              </Nav.Item>
+            </div>
+          </Accordion.Collapse>
+        </Accordion>
+
+        <Nav.Item>
+          <NavLink to="new-post" className="ps-3 py-2 d-block">
+            <i className="bi bi-pencil-square fs-5 pe-2"></i> Share a New Note
+          </NavLink>
+        </Nav.Item>
+      </Nav>
+    </nav>
   );
 }
 
@@ -55,10 +78,23 @@ function CoursesToggle({ eventKey }) {
     <Nav.Link onClick={decoratedOnClick}>
       <div className="d-flex justify-content-between">
         <span>
-          <i class="bi bi-backpack3"></i> My Courses
+          <i className="bi bi-backpack3 fs-5 pe-2"></i> My Courses
         </span>
         <i className={` bi bi-chevron-${isClicked ? 'up' : 'down'}`}></i>
       </div>
     </Nav.Link>
+  );
+}
+
+function SearchBar() {
+  return (
+    <Form>
+      <InputGroup className="px-2 mb-3">
+        <Form.Control placeholder="Search for Courses" />
+        <Button variant="outline-secondary">
+          <i className="bi bi-search"></i>
+        </Button>
+      </InputGroup>
+    </Form>
   );
 }
