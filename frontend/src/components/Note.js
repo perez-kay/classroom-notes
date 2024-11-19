a1548e6af6c96be7e9d6bd0798914e0cebea9ce5
Kayla Perez <kayla.n.perez404@gmail.com>
2024-11-19 15:50:43 -0800

    reworked the layout to fit the new app plan

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -4,19 +4,26 @@ import Button from 'react-bootstrap/esm/Button';
 import { Link } from 'react-router-dom';
 
 function Note({ id, author, course, title, description }) {
-  const shortText = description.split(' ').slice(0, 15).join(' ') + '...';
+  const shortText =
+    description.split(' ').length < 15
+      ? description
+      : description.split(' ').slice(0, 15).join(' ') + '...';
 
   return (
     <div className="col-md-4 col-sm-6 d-flex align-items-stretch">
       <Card className="w-100 m-2">
-        <Card.Body>
-          <Card.Title>{course}</Card.Title>
-          <Card.Subtitle className="text-muted">{title}</Card.Subtitle>
+        <Card.Body className="d-flex flex-column justify-content-between">
+          <div>
+            <Card.Title>{course}</Card.Title>
+            <Card.Subtitle className="text-muted pb-2">{title}</Card.Subtitle>
 
-          <Card.Text>{shortText}</Card.Text>
-          <Link to={`/notes/${id}`}>
-            <Button variant="primary">See More</Button>
-          </Link>
+            <Card.Text>{shortText}</Card.Text>
+          </div>
+          <div className="pt-2">
+            <Link to={`/notes/${id}`}>
+              <Button variant="primary">See More</Button>
+            </Link>
+          </div>
         </Card.Body>
       </Card>
     </div>
