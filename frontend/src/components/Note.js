1a9a482e891074ecf0a05835adb45cf12c64b6f6
Kayla Perez <kayla.n.perez404@gmail.com>
2024-12-04 14:30:33 -0800

    added simple delete button

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -2,12 +2,18 @@ import styles from './Note.module.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/esm/Button';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Note({ id, author, course, title, description }) {
   const shortText =
     description.split(' ').length < 15
       ? description
       : description.split(' ').slice(0, 15).join(' ') + '...';
+
+  function handleDelete() {
+    axios.delete(`/api/notes/${id}/`);
+    console.log('deleted note');
+  }
 
   return (
     <div className="col-md-4 col-sm-6 d-flex align-items-stretch">
@@ -19,10 +25,13 @@ function Note({ id, author, course, title, description }) {
 
             <Card.Text>{shortText}</Card.Text>
           </div>
-          <div className="pt-2">
+          <div className="pt-2 d-flex justify-content-between">
             <Link to={`/notes/${id}`}>
               <Button variant="primary">See More</Button>
             </Link>
+            <Button variant="danger" onClick={handleDelete}>
+              Delete
+            </Button>
           </div>
         </Card.Body>
       </Card>
